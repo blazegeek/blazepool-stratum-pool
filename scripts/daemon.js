6470a58fe569b6609c315088932ae65d34c5ed3b
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 19:41:01 -0500

    change ' to "

--- scripts/daemon.js
+++ scripts/daemon.js
@@ -5,18 +5,18 @@
  */
 
 // Import Required Modules
-var http = require('http');
-var cp = require('child_process');
-var events = require('events');
-var async = require('async');
+var http = require("http");
+var cp = require("child_process");
+var events = require("events");
+var async = require("async");
 
 /**
  * The Daemon interface interacts with the coin Daemon by using the RPC interface.
  * in order to make it work it needs, as constructor, an array of objects containing
- * - 'host'    : hostname where the coin lives
- * - 'port'    : port where the coin accepts RPC connections
- * - 'user'    : username of the coin for the RPC interface
- * - 'password': password for the RPC interface of the coin
+ * - "host"    : hostname where the coin lives
+ * - "port"    : port where the coin accepts RPC connections
+ * - "user"    : username of the coin for the RPC interface
+ * - "password": password for the RPC interface of the coin
 **/
 
 // DaemonInterface Main Function
@@ -25,18 +25,18 @@ var DaemonInterface = function(daemons, logger) {
     // Establish Private Daemon Variables
     var _this = this;
     logger = logger || function(severity, message) {
-        console.log(severity + ': ' + message);
+        console.log(severity + ": " + message);
     };
 
     // Check if All Daemons are Online
     function isOnline(callback) {
-        cmd('getpeerinfo', [], function(results) {
+        cmd("getpeerinfo", [], function(results) {
             var allOnline = results.every(function(result) {
                 return !results.error;
             });
             callback(allOnline);
             if (!allOnline) {
-                _this.emit('connectionFailed', results);
+                _this.emit("connectionFailed", results);
             }
         });
     }
@@ -56,7 +56,7 @@ var DaemonInterface = function(daemons, logger) {
     function initDaemons() {
         isOnline(function(online) {
             if (online) {
-                _this.emit('online');
+                _this.emit("online");
             }
         });
     }
@@ -66,12 +66,12 @@ var DaemonInterface = function(daemons, logger) {
 
         // Establish HTTP Options
         var options = {
-            hostname: (typeof(instance.host) === 'undefined' ? '127.0.0.1' : instance.host),
+            hostname: (typeof(instance.host) === "undefined" ? "127.0.0.1" : instance.host),
             port: instance.port,
-            method: 'POST',
-            auth: instance.user + ':' + instance.password,
+            method: "POST",
+            auth: instance.user + ":" + instance.password,
             headers: {
-                'Content-Length': jsonData.length
+                "Content-Length": jsonData.length
             }
         };
 
@@ -79,19 +79,19 @@ var DaemonInterface = function(daemons, logger) {
         var parseJson = function(res, data) {
             var dataJson;
             if ((res.statusCode === 401) || (res.statusCode === 403)) {
-                logger('error', 'Unauthorized RPC access - invalid RPC username or password');
+                logger("error", "Unauthorized RPC access - invalid RPC username or password");
                 return;
             }
             try {
                 dataJson = JSON.parse(data);
             }
             catch(e) {
-                if (data.indexOf(':-nan') !== -1) {
+                if (data.indexOf(":-nan") !== -1) {
                     data = data.replace(/:-nan,/g, ":0");
                     parseJson(res, data);
                     return;
                 }
-                logger('error', 'Could not parse RPC data from daemon instance  ' + instance.index + '\nRequest Data: ' + jsonData + '\nReponse Data: ' + data);
+                logger("error", "Could not parse RPC data from daemon instance  " + instance.index + "\nRequest Data: " + jsonData + "\nReponse Data: " + data);
 
             }
             if (dataJson) {
@@ -101,22 +101,22 @@ var DaemonInterface = function(daemons, logger) {
 
         // Establish HTTP Request
         var req = http.request(options, function(res) {
-            var data = '';
-            res.setEncoding('utf8');
-            res.on('data', function(chunk) {
+            var data = "";
+            res.setEncoding("utf8");
+            res.on("data", function(chunk) {
                 data += chunk;
             });
-            res.on('end', function() {
+            res.on("end", function() {
                 parseJson(res, data);
             });
         });
 
         // Configure Error Behavior
-        req.on('error', function(e) {
-            if (e.code === 'ECONNREFUSED')
-                callback({type: 'offline', message: e.message}, null);
+        req.on("error", function(e) {
+            if (e.code === "ECONNREFUSED")
+                callback({type: "offline", message: e.message}, null);
             else
-                callback({type: 'request error', message: e.message}, null);
+                callback({type: "request error", message: e.message}, null);
         });
 
         // Return JSON Output
