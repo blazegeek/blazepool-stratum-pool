044bc3fd99cb5bdee4d7427941ea2cc0ff5be38f
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 19:39:50 -0500

    change ' to "

--- scripts/index.js
+++ scripts/index.js
@@ -5,16 +5,16 @@
  */
 
 // Import Required Modules
-var net = require('net');
-var events = require('events');
+var net = require("net");
+var events = require("events");
 
 // Load Hashing Algorithms
-require('./algorithms.js');
+require("./algorithms.js");
 
 // Establish Main Pool Exports
-var Pool = require('./pool.js');
-exports.daemon = require('./daemon.js');
-exports.difficulty = require('./difficulty.js');
+var Pool = require("./pool.js");
+exports.daemon = require("./daemon.js");
+exports.difficulty = require("./difficulty.js");
 exports.createPool = function(poolOptions, authorizeFn) {
     var newPool = new Pool(poolOptions, authorizeFn);
     return newPool;
